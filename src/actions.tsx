d018217ebebfecb2703c2f9c775dec3b0e013caf
Mark Carson <markcarson121@gmail.com>
2024-07-31 21:37:34 -0300

    migrated project

---
+++ src/actions.tsx
@@ -0,0 +1,78 @@
+import axios from "axios";
+
+export const getBungieNetUser = (
+  membershipId: string,
+  membershipType: number,
+  apiKey: string
+) => {
+  return axios
+    .get(
+      `https://www.bungie.net/platform/User/GetMembershipsById/${membershipId}/${membershipType}/`,
+      {
+        headers: {
+          "X-API-Key": apiKey,
+        },
+      }
+    )
+    .then((response) => {
+      return response.data.Response;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+export const getCharacterEquipment = (membershipId: string, apiKey: string) => {
+  return axios
+    .get(
+      `https://www.bungie.net/Platform/Destiny2/2/Profile/${membershipId}/?components=CharacterEquipment`,
+      {
+        headers: {
+          "X-API-Key": apiKey,
+        },
+      }
+    )
+    .then((response) => {
+      return response.data.Response;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+export const getDestinyCharacters = (membershipId: string, apiKey: string) => {
+  return axios
+    .get(
+      `https://www.bungie.net/Platform/Destiny2/2/Profile/${membershipId}/?components=Characters`,
+      {
+        headers: {
+          "X-API-Key": apiKey,
+        },
+      }
+    )
+    .then((response) => {
+      return response.data.Response;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+export const getItemDetails = (itemHash: string, apiKey: string) => {
+  return axios
+    .get(
+      `https://www.bungie.net/Platform/Destiny2/Manifest/DestinyInventoryItemDefinition/${itemHash}/`,
+      {
+        headers: {
+          "X-API-Key": apiKey,
+        },
+      }
+    )
+    .then((response) => {
+      console.log("itemDetails", response.data.Response);
+      return response.data.Response;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
