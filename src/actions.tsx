2b590f43f510616b75046cbaad832c3d5602cc5a
Mark Carson <markcarson121@gmail.com>
2024-08-02 14:40:28 -0300

    refactored actions

--- src/actions.tsx
+++ src/actions.tsx
@@ -1,96 +1,48 @@
 import axios from "axios";
 
+const BASE_URL = "https://www.bungie.net/platform";
+const getHeaders = (apiKey: string) => ({
+  headers: {
+    "X-API-Key": apiKey,
+  },
+});
+
+const fetchData = (url: string, apiKey: string) => {
+  return axios
+    .get(url, getHeaders(apiKey))
+    .then((response) => {
+      return response.data.Response;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
 export const getBungieNetUser = (
   membershipId: string,
   membershipType: number,
   apiKey: string
 ) => {
-  return axios
-    .get(
-      `https://www.bungie.net/platform/User/GetMembershipsById/${membershipId}/${membershipType}/`,
-      {
-        headers: {
-          "X-API-Key": apiKey,
-        },
-      }
-    )
-    .then((response) => {
-      return response.data.Response;
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+  const url = `${BASE_URL}/User/GetMembershipsById/${membershipId}/${membershipType}/`;
+  return fetchData(url, apiKey);
 };
 
 export const getCharacterEquipment = (membershipId: string, apiKey: string) => {
-  return axios
-    .get(
-      `https://www.bungie.net/Platform/Destiny2/2/Profile/${membershipId}/?components=CharacterEquipment`,
-      {
-        headers: {
-          "X-API-Key": apiKey,
-        },
-      }
-    )
-    .then((response) => {
-      return response.data.Response;
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+  const url = `${BASE_URL}/Destiny2/2/Profile/${membershipId}/?components=CharacterEquipment`;
+  return fetchData(url, apiKey);
 };
 
 export const getClassDefinition = (classHash: number, apiKey: string) => {
-  return axios
-    .get(
-      `https://www.bungie.net/Platform/Destiny2/Manifest/DestinyClassDefinition/${classHash}/`,
-      {
-        headers: {
-          "X-API-Key": apiKey,
-        },
-      }
-    )
-    .then((response) => {
-      return response.data.Response;
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+  const url = `${BASE_URL}/Destiny2/Manifest/DestinyClassDefinition/${classHash}/`;
+  return fetchData(url, apiKey);
 };
 
 export const getDestinyCharacters = (membershipId: string, apiKey: string) => {
-  return axios
-    .get(
-      `https://www.bungie.net/Platform/Destiny2/2/Profile/${membershipId}/?components=Characters`,
-      {
-        headers: {
-          "X-API-Key": apiKey,
-        },
-      }
-    )
-    .then((response) => {
-      return response.data.Response;
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+  const url = `${BASE_URL}/Destiny2/2/Profile/${membershipId}/?components=Characters`;
+  return fetchData(url, apiKey);
 };
 
-export const getItemDetails = (itemHash: string, apiKey: string) => {
-  return axios
-    .get(
-      `https://www.bungie.net/Platform/Destiny2/Manifest/DestinyInventoryItemDefinition/${itemHash}/`,
-      {
-        headers: {
-          "X-API-Key": apiKey,
-        },
-      }
-    )
-    .then((response) => {
-      console.log("itemDetails", response.data.Response);
-      return response.data.Response;
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+export const getItemDetails = (itemHash: number, apiKey: string) => {
+  const url = `${BASE_URL}/Destiny2/Manifest/DestinyInventoryItemDefinition/${itemHash}/`;
+  return fetchData(url, apiKey);
 };
