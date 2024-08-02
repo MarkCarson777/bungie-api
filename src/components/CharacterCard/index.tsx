42fadc62017e5aafe35aec39c67ff3a0793d610e
Mark Carson <markcarson121@gmail.com>
2024-08-02 14:23:05 -0300

    added pending state to CharacterCard to load class icons

--- src/components/CharacterCard/index.tsx
+++ src/components/CharacterCard/index.tsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 
+import { Icon } from "../Icon";
+
 import { getClassDefinition } from "../../actions";
 
 type CharacterCardProps = {
@@ -11,26 +13,27 @@ type CharacterCardProps = {
 
 export function CharacterCard(props: CharacterCardProps) {
   const { character } = props;
-  const [characterClass, setCharacterClass] = useState<string | null>(null);
+  const [characterClass, setCharacterClass] = useState<string>("");
+  const [pending, setPending] = useState<boolean>(false);
   const apiKey = import.meta.env.VITE_BUNGIE_API_KEY as string;
 
   useEffect(() => {
+    setPending(true);
     const getCharacterClass = async () => {
       try {
         const response = await getClassDefinition(character.classHash, apiKey);
-        console.log("class", response);
         setCharacterClass(response.displayProperties.name);
       } catch (error) {
         console.error(error);
       }
     };
 
-    getCharacterClass();
+    getCharacterClass().finally(() => setPending(false));
   }, []);
 
   return (
     <div className="flex justify-center items-center h-24 w-24 border-2">
-      <span>{characterClass}</span>
+      {pending ? <span>Loading...</span> : <Icon icon={characterClass} />}
     </div>
   );
 }
